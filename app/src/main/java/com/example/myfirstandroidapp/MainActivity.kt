b9e77e1a0b85641774eb21cef34b356d01f3b95b
mahima shrestha <mahimashrestha43@gmail.com>
2021-06-07 11:51:00 +0545

    Initial commit

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myfirstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var et:EditText
+    private lateinit var et1:EditText
+    private lateinit var btnAdd:Button
+    private lateinit var tvresult:TextView
+    private lateinit var rdsum:RadioButton
+    private lateinit var rdsub:RadioButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         et=findViewById(R.id.et)
+         et1=findViewById(R.id.et1)
+         tvresult=findViewById(R.id.tvresult)
+         btnAdd=findViewById(R.id.btnAdd)
+        rdsum=findViewById(R.id.rdsum)
+            rdsub=findViewById(R.id.rdsub)
+
+        btnAdd.setOnClickListener {
+            val first:Int=et.text.toString().toInt()
+            val second:Int=et1.text.toString().toInt()
+//text validation
+            if(TextUtils.isEmpty(et.text)){
+                et.error="Enter first num"
+                et.requestFocus()
+                return@setOnClickListener
+            }
+            else if(TextUtils.isEmpty(et1.text)){
+                et1.error="Enter second num"
+                et1.requestFocus()
+                return@setOnClickListener
+            }
+            if(rdsum.isChecked){
+                addNum(first,second)
+            }else{
+                subNum(first, second)
+            }
+        }
+    }
+    private fun addNum(first:Int,second:Int){
+        val result=first+second
+        tvresult.text=result.toString()
+        Toast.makeText(this@MainActivity,"Sum is $result",Toast.LENGTH_SHORT).show();
+    }
+    private fun subNum(first:Int,second:Int){
+        val result=first-second
+        tvresult.text=result.toString()
+        Toast.makeText(this@MainActivity,"Subtraction is $result",Toast.LENGTH_SHORT).show();
+    }
+}
